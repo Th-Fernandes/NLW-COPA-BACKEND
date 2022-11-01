02f2b567f1cf91fa3bbfc9302516c87daf9e6b0d
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-11-01 09:19:12 -0300

    Pool table

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
 import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient({ log: ["query"] });
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
-  fastify.get("/pools/count", () => {
-    return {
-      counter: 13,
-    };
+  fastify.get("/pools/count", async () => {
+    const poolsCount = await prisma.pool.count();
+    return { poolsCount };
   });
 
   await fastify.listen({ port: 3333 });
