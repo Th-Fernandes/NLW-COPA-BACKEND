6e8059a8be308efc54f6724b5c36bf45b5a25005
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-11-01 11:26:52 -0300

    initial config

--- src/server.ts
+++ src/server.ts
@@ -14,11 +14,11 @@ async function bootstrap() {
   });
 
   fastify.get("/pools/count", async () => {
-    const poolsCount = await prisma.pool.count();
-    return { poolsCount };
+    const count = await prisma.pool.count();
+    return { count };
   });
 
-  await fastify.listen({ port: 3333, host: "0.0.0.0" });
+  await fastify.listen({ port: 3333 /*host: "0.0.0.0"*/ });
 }
 
 bootstrap();
